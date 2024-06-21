eb1bbfea4e8366a639a809e95f4ce4e6bc123061
Ekaterina Vodolazhskaia <katik_v@inbox.ru>
2024-06-21 10:24:49 +0300

    added extended logging

--- src/test/java/specs/ReqresApiSpec.java
+++ src/test/java/specs/ReqresApiSpec.java
@@ -6,39 +6,32 @@ import io.restassured.specification.ResponseSpecification;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.BODY;
-import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.filter.log.LogDetail.*;
 import static io.restassured.http.ContentType.JSON;
 
 public class ReqresApiSpec {
     public static RequestSpecification requestSpec = with()
             .filter(withCustomTemplates())
-            .log().uri()
-            .log().body()
-            .log().headers()
+            .log().all()
             .contentType(JSON);
 
     public static ResponseSpecification successful200ResponseSpec = new ResponseSpecBuilder()
             .expectStatusCode(200)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
 
     public static ResponseSpecification successful204ResponseSpec = new ResponseSpecBuilder()
             .expectStatusCode(204)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
 
     public static ResponseSpecification error400ResponseSpec = new ResponseSpecBuilder()
             .expectStatusCode(400)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
     public static ResponseSpecification error404ResponseSpec = new ResponseSpecBuilder()
             .expectStatusCode(404)
-            .log(STATUS)
-            .log(BODY)
+            .log(ALL)
             .build();
 
 
